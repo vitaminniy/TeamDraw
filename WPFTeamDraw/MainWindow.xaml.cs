d25e2c9f4b632c273cdb251d9246a440011ccc4e
Udalov <re.udalov@gmail.com>
2014-12-19 20:19:15 +0300

    New WPF solution, base code for drawing

---
+++ WPFTeamDraw/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTeamDraw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private HashMap<Polyline, long> plines = new HashMap<Polyline,long>();
+
+        private Polyline _pl;
+        private bool _isDrawing = false;
+
+        private void DrawArea_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                _isDrawing = true;
+                _pl = new Polyline();
+                Point p = e.GetPosition(this);
+                p.Y -= 100;
+                _pl.Points.Add(p);
+                _pl.Stroke = new SolidColorBrush(Colors.Black);
+                _pl.StrokeThickness = 5;
+                DrawArea.Children.Add(_pl);
+            }
+        }
+
+        private void DrawArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && _isDrawing)
+            {
+                Point p = e.GetPosition(this);
+                p.Y -= 100;
+                _pl.Points.Add(p);
+            }
+        }
+
+        private void DrawArea_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDrawing)
+            {
+                _isDrawing = false;
+                //Send Line to server
+            }
+        }
+
+        public void DrawLineSync(RPoint rline)
+        {
+            
+        }
+        
+    }
+}
---
+++ WPFTeamDraw/Util.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTeamDraw
+{
+    public class Util
+    {
+
+        private static readonly DateTime Jan1st1970 = new DateTime
+            (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        private static long _serverTimeDifference = 0;
+
+        public static long ServerTimeDifference
+        {
+            get { return _serverTimeDifference; }
+            set { _serverTimeDifference = value; }
+        }
+
+        public static long CurrentTimeMillis()
+        {
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+
+        public static long ServerTimeMillis()
+        {
+            return CurrentTimeMillis() + ServerTimeDifference;
+        }
+    }
+}
